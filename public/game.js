00f18939a28c7f79ed005770225ce2294538035f
arc1773 <123033839+arc1773@users.noreply.github.com>
2024-08-16 00:44:39 +0200

    1.0.5

--- public/game.js
+++ public/game.js
@@ -3,198 +3,153 @@ const socket = io();
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 500;
+canvas.height = 500;
 
 var wc = canvas.width;
 var hc = canvas.height;
 
-var game_data = {};
-var previousPositions = { x: 0, y: 0 };
-var currentPositions = { x: 0, y: 0 };
-var interpolationFactor = 0.9; // Im większa wartość, tym płynniejszy ruch
+var gap_between_lines = 50;
+var map_size = 10000;
 
-var mouse_position = {
-  x: 275,
-  y: 375,
-};
+var players_data = {};
+var meals_data = {};
 
-var move = false;
-
-var in_game = false;
-var is_collision = false;
-
-document.getElementById("play").addEventListener("click", () => {
-  in_game = true;
-  document.getElementById("play").style.display = "none";
-  socket.emit("in_game", in_game);
+socket.on("init", (data) => {
+  for (var i in data.meal) {
+    console.log(1);
+    meals_data[i] = data.meal[i];
+  }
 });
 
-socket.on("move", (data) => {
-  game_data = data;
-  previousPositions = currentPositions;
-  currentPositions = game_data.clients;
-  //resizeCanvas()
+socket.on("update", function (data) {
+  players_data = data.player;
 });
-player_h = 50;
-player_w = 50;
+
+socket.on("remove", (data) => {
+  for (var i in data.meal) {
+    delete meals_data[data.meal[i]];
+  }
+});
 
 function resizeCanvas() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   wc = canvas.width;
   hc = canvas.height;
-  document.getElementById("play").style.left = wc / 2 - 100 + "px";
-  document.getElementById("play").style.top = hc / 2 + "px";
+  socket.emit("change_canvas_size", { x: wc, y: hc });
 }
 
-
-
-function interpolatePosition() {
-  for (let clientId in game_data.clients) {
-    let interpolatedPosition = {
-      x: previousPositions[clientId].position.x + (currentPositions[clientId].position.x - previousPositions[clientId].position.x) * interpolationFactor,
-      y: previousPositions[clientId].position.y + (currentPositions[clientId].position.y - previousPositions[clientId].position.y) * interpolationFactor
-    };
-    console.log(interpolatedPosition)
-    game_data.clients[clientId].position.x=interpolatedPosition.x
-    game_data.clients[clientId].position.y=interpolatedPosition.y
-    
-  }  
-  
+function draw_player(position, size) {
+  let player = players_data[socket.id].position;
+  let drawX = position.x - (player.x - wc / 2);
+  let drawY = position.y - (player.y - hc / 2);
+  ctx.fillStyle = "black";
+  ctx.fillRect(drawX - size / 2, drawY - size / 2, size, size);
 }
 
-function board() {
-  ctx.fillStyle = "grey";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
-
-function draw_players() {
-  let currentPlayer = game_data.clients[socket.id];
-  let pp = currentPlayer.position;
-  for (const clientId in game_data.clients) {
-    let client = game_data.clients[clientId];
-    if (client.in_game) {
-      let drawX = client.position.x - (pp.x - wc / 2);
-      let drawY = client.position.y - (pp.y - hc / 2);
-      //drawPolygon(drawX, drawY, client.size, client.angles, client.angle, client.color);
-      drawCircle_t(drawX, drawY, 20, "pink");
-    }
-  }
-}
-
-function draw_meal() {
-  let pp = game_data.clients[socket.id].position;
-  let ptx = pp.x - wc / 2;
-  let pty = pp.y - hc / 2;
-  for (let c in game_data.meal) {
-    let m = game_data.meal[c];
-    ctx.save();
-    ctx.translate(m.x - ptx, m.y - pty);
-    drawCircle(5, m.type);
-    ctx.restore();
-  }
+function draw_meal(data) {
+  let player_p = players_data[socket.id].position;
+  let drawX = data.position.x - (player_p.x - wc / 2);
+  let drawY = data.position.y - (player_p.y - hc / 2);
+  ctx.fillStyle = data.color;
+  ctx.fillRect(drawX, drawY, 7, 7);
 }
 
 function draw_map() {
-  let height_of_map = 100;
-  let width_of_map = 100;
-  ctx.lineWidth = 2;
-  ctx.strokeRect(wc - width_of_map, 0, width_of_map, height_of_map);
-  ctx.fillStyle = "grey";
-  ctx.fillRect(wc - width_of_map, 0, width_of_map, height_of_map);
-
-  //draw players on the map
-  for (const client in game_data.clients) {
-    p = game_data.clients[client];
-
-    if (p.in_game) {
-      //ctx.save();
-      //ctx.translate(wc - width_of_map + p.position.x / 50, p.position.y / 50);
-      //ctx.rotate(p.angle);
-      //drawPolygon(4, p.angles, p.color);
-      //ctx.restore();
-      drawCircle_t(
-        wc - width_of_map + p.position.x / 50,
-        p.position.y / 50,
-        2,
-        "pink"
-      );
+  let player_p = players_data[socket.id].position;
+  ctx.fillStyle = "#E6E8E6";
+  ctx.fillRect(0, 0, wc, hc);
+  if (player_p) {
+    ctx.strokeStyle = "#B8BAC8";
+    ctx.lineWidth = 1;
+    for (let i = 0; i <= map_size / gap_between_lines; i++) {
+      let start_drawX = gap_between_lines * i - (player_p.x - wc / 2);
+      let start_drawY = 0 - (player_p.y - hc / 2);
+      let stop_drawX = gap_between_lines * i - (player_p.x - wc / 2);
+      let stop_drawY = map_size - (player_p.y - hc / 2);
+      ctx.beginPath();
+      ctx.moveTo(start_drawX, start_drawY);
+      ctx.lineTo(stop_drawX, stop_drawY);
+      ctx.stroke();
+    }
+    for (let i = 0; i <= map_size / gap_between_lines; i++) {
+      let start_drawX = 0 - (player_p.x - wc / 2);
+      let start_drawY = gap_between_lines * i - (player_p.y - hc / 2);
+      let stop_drawX = map_size - (player_p.x - wc / 2);
+      let stop_drawY = gap_between_lines * i - (player_p.y - hc / 2);
+      ctx.beginPath();
+      ctx.moveTo(start_drawX, start_drawY);
+      ctx.lineTo(stop_drawX, stop_drawY);
+      ctx.stroke();
     }
   }
 }
 
-var angle = 0;
+function draw_minimap() {
+  ctx.fillStyle = "#E6E8E6";
+  ctx.fillRect(wc - 100, 0, 100, 100);
+  ctx.strokeStyle = "red";
+  ctx.strokeRect(wc - 100, 0, 100, 100);
 
-function draw() {
-  board();
-  if (in_game) {
-    draw_meal();
-    draw_players();
-    draw_map();
+  for (var i in players_data) {
+    let player = players_data[i];
+    ctx.fillStyle = "black";
+    ctx.fillRect(
+      player.position.x / 100 + (wc - 100),
+      player.position.y / 100,
+      2,
+      2
+    );
   }
 }
 
-function play() {
-  interpolatePosition()
-  //give_to_get_data();
-  //send_collision_info();
+setInterval(function () {
   resizeCanvas();
-  send_new_position();
-  draw();
-}
+  ctx.clearRect(0, 0, 500, 500);
+  if (players_data[socket.id] != null) {
+    draw_map();
+    for (var i in meals_data) {
+      draw_meal(meals_data[i]);
+    }
+    for (var i in players_data) {
+      draw_player(players_data[i].position, players_data[i].parametrs.size);
+    }
+    draw_minimap();
+  }
+}, 40);
 
-setInterval(play, 1000 / 120);
+document.onkeydown = function (event) {
+  if (event.keyCode === 68) {
+    socket.emit("keyPress", { inputId: "right", state: true });
+  } else if (event.keyCode === 83) {
+    socket.emit("keyPress", { inputId: "up", state: true });
+  } else if (event.keyCode === 65) {
+    socket.emit("keyPress", { inputId: "left", state: true });
+  } else if (event.keyCode === 87) {
+    socket.emit("keyPress", { inputId: "down", state: true });
+  }
+};
+document.onkeyup = function (event) {
+  if (event.keyCode === 68) {
+    socket.emit("keyPress", { inputId: "right", state: false });
+  } else if (event.keyCode === 83) {
+    socket.emit("keyPress", { inputId: "up", state: false });
+  } else if (event.keyCode === 65) {
+    socket.emit("keyPress", { inputId: "left", state: false });
+  } else if (event.keyCode === 87) {
+    socket.emit("keyPress", { inputId: "down", state: false });
+  }
+};
 
 canvas.addEventListener("click", (event) => {
-  move = !move;
+  socket.emit("stop_start_move");
 });
 
 canvas.addEventListener("mousemove", (event) => {
-  mouse_position = {
+  let mouse_position = {
     x: event.clientX,
     y: event.clientY,
   };
+  socket.emit("mouse_position", mouse_position);
 });
-
-//function give_to_get_data(){
-//  socket.emit("give_to_get_data", {});
-//}
-
-setInterval(send_new_position, 1);
-
-function send_new_position() {
-  let inf_to_new_position = {
-    mouse_position: mouse_position,
-    wc: wc,
-    hc: hc,
-    move: move,
-  };
-  //let inf_to_new_position={}
-  socket.emit("move", inf_to_new_position);
-}
-function send_collision_info() {
-  socket.emit("collision", is_collision);
-}
-
-function calculate_collision() {
-  for (let p in game_data.clients) {
-    if (p != socket.id) {
-      const fe = game_data.clients[socket.id];
-      const se = game_data.clients[p];
-      is_collision = checkPolygonsCollision(
-        fe.size,
-        fe.position,
-        fe.angle,
-        fe.angles,
-        se.size,
-        se.position,
-        se.angle,
-        se.angles
-      );
-      if (is_collision) {
-        break;
-      }
-    }
-  }
-}
--- server.js
+++ server.js
@@ -1,306 +1,181 @@
-// Funkcja generująca wierzchołki wielokąta
-function generatePolygonVertices({ x, y }, size, sides, angle) {
-  const vertices = [];
-  const angleStep = (2 * Math.PI) / sides;
-  for (let i = 0; i < sides; i++) {
-    const currentAngle = angle + i * angleStep;
-    vertices.push({
-      x: x + size * Math.cos(currentAngle),
-      y: y + size * Math.sin(currentAngle),
-    });
-  }
-  return vertices;
-}
-
-// Funkcja do sprawdzania kolizji między dwoma wielokątami
-function polygonsCollision(poly1, poly2) {
-  const getAxes = (poly) =>
-    poly.map((_, i) => {
-      const next = poly[(i + 1) % poly.length];
-      return { x: next.y - poly[i].y, y: poly[i].x - next.x };
-    });
-
-  const project = (axis, poly) => poly.map((v) => v.x * axis.x + v.y * axis.y);
-  const overlap = (proj1, proj2) =>
-    Math.max(...proj1) >= Math.min(...proj2) &&
-    Math.max(...proj2) >= Math.min(...proj1);
-
-  const axes = [...getAxes(poly1), ...getAxes(poly2)];
-  return axes.every((axis) =>
-    overlap(project(axis, poly1), project(axis, poly2))
-  );
-}
-
-// Główna funkcja sprawdzająca kolizję na podstawie parametrów wielokątów
-function checkPolygonsCollision(
-  size1,
-  position1,
-  angle1,
-  sides1,
-  size2,
-  position2,
-  angle2,
-  sides2
-) {
-  const poly1 = generatePolygonVertices(position1, size1, sides1, angle1);
-  const poly2 = generatePolygonVertices(position2, size2, sides2, angle2);
-  return polygonsCollision(poly1, poly2);
-}
-function generatePolygonVertices({ x, y }, size, sides, angle) {
-  const vertices = [];
-  const angleStep = (2 * Math.PI) / sides;
-  for (let i = 0; i < sides; i++) {
-    const currentAngle = angle + i * angleStep;
-    vertices.push({
-      x: x + size * Math.cos(currentAngle),
-      y: y + size * Math.sin(currentAngle),
-    });
-  }
-  return vertices;
-}
-
-function circlePolygonCollision(circle, polygon) {
-  const vertices = generatePolygonVertices(
-    polygon.position,
-    polygon.size,
-    polygon.angles,
-    polygon.angle
-  );
-
-  // Sprawdzenie kolizji krawędzi wielokąta z okręgiem
-  for (let i = 0; i < vertices.length; i++) {
-    const start = vertices[i];
-    const end = vertices[(i + 1) % vertices.length];
-
-    if (lineCircleCollision(start, end, circle.position, circle.radius)) {
-      return true;
-    }
-  }
-
-  // Sprawdzenie, czy środek koła jest wewnątrz wielokąta
-  if (isPointInPolygon(circle.position, vertices)) {
-    return true;
-  }
-
-  return false;
-}
-
-function lineCircleCollision(start, end, circle, radius) {
-  const ax = start.x - circle.x;
-  const ay = start.y - circle.y;
-  const bx = end.x - circle.x;
-  const by = end.y - circle.y;
-
-  const a = (bx - ax) ** 2 + (by - ay) ** 2;
-  const b = 2 * (ax * (bx - ax) + ay * (by - ay));
-  const c = ax ** 2 + ay ** 2 - radius ** 2;
-
-  let disc = b ** 2 - 4 * a * c;
-  if (disc <= 0) return false;
-  disc = Math.sqrt(disc);
-
-  const t1 = (-b - disc) / (2 * a);
-  const t2 = (-b + disc) / (2 * a);
-
-  if ((t1 >= 0 && t1 <= 1) || (t2 >= 0 && t2 <= 1)) return true;
-  return false;
-}
-
-function isPointInPolygon(point, vertices) {
-  let collision = false;
-  for (let i = 0, j = vertices.length - 1; i < vertices.length; j = i++) {
-    const vi = vertices[i];
-    const vj = vertices[j];
-    if (
-      vi.y > point.y !== vj.y > point.y &&
-      point.x < ((vj.x - vi.x) * (point.y - vi.y)) / (vj.y - vi.y) + vi.x
-    ) {
-      collision = !collision;
-    }
-  }
-  return collision;
-}
-const { randomInt } = require("crypto");
-//Server
 const express = require("express");
 const http = require("http");
+const { emit } = require("process");
 const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-var game_data = {
-  clients: {},
-  meal: [],
-};
+var players_data = {}; //123123:{...},
+var meals_data = {};
 
 app.use(express.static("public"));
 
+server.listen(443);
+console.log("server started(listan on port 443");
+
 io.on("connection", (socket) => {
   console.log("a user connected");
-  game_data.clients[socket.id] = {
+  players_data[socket.id] = {
     position: {
-      x: 275,
-      y: 375,
+      x: 5000,
+      y: 5000,
     },
-    velocity:{
-      x:1,
-      y:1
+    mouse_position: {
+      x: 250,
+      y: 250,
     },
-    in_game: false,
-    size: 10,
-    move_speed: 1,
-    rotation_speed: 0.02,
-    angles: 3,
-    counter_to_angles: 0,
+    screen_size: {
+      x: 500,
+      y: 500,
+    },
+    parametrs: {
+      move_speed: 3,
+      size: 20,
+      number_of_angles: 3,
+      rotation_speed: 1,
+    },
+    moving: false,
     angle: 0,
-    color: "pink",
+    spdX: 0,
+    spdY: 0,
   };
+  socket.emit("init", { meal: meals_data });
 
-  socket.on("move", (data) => {
-    if(game_data.clients[socket.id].in_game){
-      if (data.move) {
-        game_data.clients[socket.id].velocity.x =game_data.clients[socket.id].move_speed*((data.mouse_position.x-(data.wc/2))/(data.wc/2));
-        game_data.clients[socket.id].velocity.y =game_data.clients[socket.id].move_speed*((data.mouse_position.y-(data.hc/2))/(data.hc/2));
-        game_data.clients[socket.id].position.x +=game_data.clients[socket.id].move_speed*((data.mouse_position.x-(data.wc/2))/(data.wc/2));
-        game_data.clients[socket.id].position.y +=game_data.clients[socket.id].move_speed*((data.mouse_position.y-(data.hc/2))/(data.hc/2));
-      }
-    }
-  });
-  //socket.on("give_to_get_data", (data) => {
-  //  socket.emit("move", game_data);
-  //});
-
-  socket.on("in_game", (data) => {
-    game_data.clients[socket.id].in_game = data;
+  socket.on("stop_start_move", function () {
+    players_data[socket.id].moving = !players_data[socket.id].moving;
   });
 
-  //socket.on("collision", (data) => {
-  //  if (data) {
-  //    game_data.clients[socket.id].color = "red";
-  //  } else {
-  //    game_data.clients[socket.id].color = "pink";
-  //  }
-  //});
+  socket.on("mouse_position", function (data) {
+    players_data[socket.id].mouse_position = data;
+  });
+
+  socket.on("change_canvas_size", function (data) {
+    players_data[socket.id].screen_size = data;
+  });
 
   socket.on("disconnect", () => {
-    delete game_data.clients[socket.id];
+    delete players_data[socket.id];
     console.log("user disconnected");
   });
 });
 
-function server_loop() {
-  send_game_data()
-  counters_of_characteristic();
-  change_angle();
-  check_colision();
-  
-}
-setInterval(server_loop, 1);
-
-function send_game_data() {
-  io.emit("move", game_data);
+function areSquaresColliding(square2, square1) {
+  return !(
+    (
+      square1.x + square1.size < square2.x || // Kwadrat 1 jest po lewej stronie kwadratu 2
+      square1.x > square2.x + square2.size || // Kwadrat 1 jest po prawej stronie kwadratu 2
+      square1.y + square1.size < square2.y || // Kwadrat 1 jest powyżej kwadratu 2
+      square1.y > square2.y + square2.size
+    ) // Kwadrat 1 jest poniżej kwadratu 2
+  );
 }
 
-//setInterval(send_game_data, 1)
-function change_angle() {
-  for (let p in game_data.clients) {
-    game_data.clients[p].angle += game_data.clients[p].rotation_speed;
-  }
-}
-
-function check_colision() {
-  for (let f in game_data.clients) {
-    let fe = game_data.clients[f];
-    //with player
-    for (let s in game_data.clients) {
+function players_update() {
+  var pack = {};
+  for (var i in players_data) {
+    var player = players_data[i];
+    //colision
+    for (var n in meals_data) {
+      var meal = meals_data[n];
       if (
-        f != s &&
-        game_data.clients[s].in_game &&
-        game_data.clients[f].in_game
+        areSquaresColliding(
+          {
+            x: player.position.x - player.parametrs.size / 2,
+            y: player.position.y - player.parametrs.size / 2,
+            size: player.parametrs.size,
+          },
+          {
+            x: meal.position.x - 7 / 2,
+            y: meal.position.y - 7 / 2,
+            size: 7,
+          }
+        )
       ) {
-        let se = game_data.clients[s];
-        let col = checkPolygonsCollision(
-          fe.size,
-          fe.position,
-          fe.angle,
-          fe.angles,
-          se.size,
-          se.position,
-          se.angle,
-          se.angles
-        );
-        if (col) {
-          fe.color = "red";
-          se.color = "red";
-          break;
-        } else {
-          fe.color = "pink";
-          se.color = "pink";
+        if (meals_data[n].color == "red") {
+          player.parametrs.size += 1;
+        } else if (meals_data[n].color == "pink") {
+          player.parametrs.move_speed += 1;
         }
+
+        delete meals_data[n];
+        meals_to_remove.push(n);
       }
     }
-    //with meal
-    for (let c in game_data.meal) {
-      let meal = game_data.meal[c];
-      const circle = {
-        position: { x: meal.x, y: meal.y },
-        radius: 5,
-      };
-      const polygon = {
-        position: { x: fe.position.x, y: fe.position.y },
-        size: fe.size,
-        angles: fe.angles,
-        angle: fe.angle,
-      };
-      let collision_c = circlePolygonCollision(circle, polygon);
-      if (collision_c) {
-        if (meal.type == "green") {
-          fe.counter_to_angles += 1;
-        } else if (meal.type == "yellow") {
-          fe.move_speed += 1;
-        } else if (meal.type == "purple") {
-          fe.rotation_speed += 0.07;
-        }
-        game_data.meal.splice(c, 1);
+
+    //move
+    if (player.moving) {
+      var mouse_positon = player.mouse_position;
+      if (mouse_positon.x > player.screen_size.x * 0.75) {
+        mouse_positon.x = player.screen_size.x * 0.75;
+      } else if (mouse_positon.x < player.screen_size.x * 0.25) {
+        mouse_positon.x = player.screen_size.x * 0.25;
       }
+      if (mouse_positon.y > player.screen_size.y * 0.75) {
+        mouse_positon.y = player.screen_size.y * 0.75;
+      } else if (mouse_positon.y < player.screen_size.y * 0.25) {
+        mouse_positon.y = player.screen_size.y * 0.25;
+      }
+
+      player.spdX =
+        player.parametrs.move_speed *
+        ((mouse_positon.x - player.screen_size.x / 2) /
+          (player.screen_size.x / 4));
+      player.spdY =
+        player.parametrs.move_speed *
+        ((mouse_positon.y - player.screen_size.y / 2) /
+          (player.screen_size.y / 4));
+
+      player.position.x += player.spdX;
+      player.position.y += player.spdY;
     }
+    pack[i] = {
+      position: player.position,
+      parametrs: player.parametrs,
+      angle: player.angle,
+    };
+  }
+  return pack;
+}
+
+var meals_to_add = {};
+var meals_to_remove = [];
+
+function meals_update() {
+  //spawn
+  //if(meals_data.lenth<=100){
+  if (true) {
+    let id_of_meal = Math.random();
+    let position = {
+      x: 10000 * Math.random(),
+      y: 10000 * Math.random(),
+    };
+    let color_of_meal = "pink";
+    let chance = .5;
+    if (position.x>5000) {
+      chance = .25
+    }else{
+      chance = .75
+    }
+    if (Math.random() > chance) {
+      color_of_meal = "red";
+    }
+    meals_data[id_of_meal] = {
+      position: position,
+      color: color_of_meal,
+    };
+    meals_to_add[id_of_meal] = meals_data[id_of_meal];
   }
 }
 
-function counters_of_characteristic() {
-  for (let c in game_data.clients) {
-    let client = game_data.clients[c];
-    if (client.counter_to_angles >= 5) {
-      client.counter_to_angles = 0;
-      client.angles += 1;
-    }
-  }
-}
-
-function spawn_meal() {
-  if (game_data.meal.length <= 100) {
-    let meal_x = Math.random() * 5000;
-    let meal_y = Math.random() * 5000;
-    let meal_type = Math.random();
-
-    if (meal_type >= 0.66) {
-      meal_type = "yellow";
-    } else if (meal_type < 0.33) {
-      meal_type = "green";
-    } else if (meal_type >= 0.33) {
-      meal_type = "purple";
-    }
-    game_data.meal.push({
-      x: meal_x,
-      y: meal_y,
-      type: meal_type,
-    });
-  }
-}
-setInterval(spawn_meal, 500);
-
-server.listen(443, () => {
-  console.log("listening on *:443");
-});
+setInterval(function () {
+  meals_update();
+  io.emit("init", { meal: meals_to_add });
+  io.emit("update", {
+    player: players_update(),
+  });
+  io.emit("remove", { meal: meals_to_remove });
+  meals_to_add = {};
+  meals_to_remove = [];
+}, 1000 / 25);
